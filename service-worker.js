c95750595506efa8c3544ef3c306b9089926a6bf
rieya1111 <riyasankpal30@gmail.com>
2026-03-13 11:54:08 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const cacheName = 'checklist-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
